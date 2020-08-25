bdb643f666ef3e7f4458ebaa52f1d2d5ae7a1f1b
Yusuf Alp <alpyusuf007@gmail.com>
2020-08-25 07:06:23 -0700

    Added a service to articles database

---
+++ blogful-scripts/seed.blogful.sql
@@ -0,0 +1,21 @@
+INSERT INTO blogful_articles(title, date_published, content) 
+ VALUES 
+ ('Title 1', now() - '21 days'::INTERVAL, 'Content 1'),
+ ('Title 2', now() - '20 days'::INTERVAL, 'Content 2'),
+ ('Title 3', now() - '19 days'::INTERVAL, 'Content 3'),
+ ('Title 4', now() - '18 days'::INTERVAL, 'Content 4'),
+ ('Title 5', now() - '17 days'::INTERVAL, 'Content 5'),
+ ('Title 6', now() - '16 days'::INTERVAL, 'Content 6'),
+ ('Title 7', now() - '15 days'::INTERVAL, 'Content 7'),
+ ('Title 8', now() - '14 days'::INTERVAL, 'Content 8'),
+ ('Title 9', now() - '13 days'::INTERVAL, 'Content 9'),
+ ('Title 10',now() - '12 days'::INTERVAL, 'Content 10'),
+ ('Title 11',now() - '11 days'::INTERVAL, 'Content 11'),
+ ('Title 12',now() - '10 days'::INTERVAL, 'Content 12'),
+ ('Title 13',now() - '9 days'::INTERVAL, 'Content 13'),
+ ('Title 14',now() - '8 days'::INTERVAL, 'Content 14'),
+ ('Title 15',now() - '7 days'::INTERVAL, 'Content 15'),
+ ('Title 16',now() - '6 days'::INTERVAL, 'Content 16'),
+ ('Title 17',now() - '5 days'::INTERVAL, 'Content 17'),
+ ('Title 18',now() - '4 days'::INTERVAL, 'Content 18'),
+ ('Title 19',now() - '3 days'::INTERVAL, 'Content 19');
